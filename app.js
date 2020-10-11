183cd25590efb8d83c9c2add45a23f0592caa451
andrew karpman <andruha1067@gmail.com>
2020-10-11 16:37:47 -0700

    resolving port issue by using heroku port setup from heroku site

--- app.js
+++ app.js
@@ -133,15 +133,11 @@ app.post("/delete", function(req,res) {
   }
 });
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started.");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 3000;
-// }
-//
-// app.listen(port, function() {
-//   console.log("Server started on port: ", port);
-// });
+app.listen(port, function() {
+  console.log("Server started on port: ", port);
+});
